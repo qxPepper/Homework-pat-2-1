b0ed4f54ba2ba0b3ee3a45011e1a8d2e691b47f9
Vitaly Gromov <vitaly.gro@gmail.com>
2021-08-23 22:39:11 +0300

    Первоначальная версия Homework-pat-2-1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 17));
+        System.out.println(intsCalc.mult(8, 4));
+        System.out.println(intsCalc.pow(2, 10));
+    }
+}
